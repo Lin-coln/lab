ec91e56305eebed4cd3e05e68fb22f8835d6e1b0
lincoln <1043156036@qq.com>
2026-01-21 00:41:17 +0800

    chore: init

---
+++ apps/view/src/App.tsx
@@ -0,0 +1,18 @@
+import { Button } from "ui";
+
+export default function App() {
+  return (
+    <div className="flex flex-col justify-center items-center min-h-screen">
+      <div className="max-w-120 mb-8">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab, blanditiis, distinctio dolore doloribus eaque
+        earum facere id ipsam magni nam natus nobis omnis optio qui quia quis similique soluta. Perferendis?
+      </div>
+      <Button
+        label={"button"}
+        onClick={() => {
+          console.log(123);
+        }}
+      />
+    </div>
+  );
+}
