8e900449a87c39d7ff6a034d7b36b7651d1c12eb
lincoln <1043156036@qq.com>
2026-01-21 01:53:39 +0800

    feat: electron build.ts

--- apps/electron/scripts/build.ts
+++ apps/electron/scripts/build.ts
@@ -1,47 +1,46 @@
 import fs from "node:fs";
 import path from "node:path";
 
-const args: Record<string, string | number | boolean> = Object.fromEntries(
-  process.argv
-    .slice(2)
-    .filter((x) => x.startsWith("--"))
-    .map((x) => {
-      let [k, v] = x.split("=");
-      v ??= "true";
-      try {
-        v = JSON.parse(v);
-      } catch {}
-      return [k!.slice(2), v as any];
-    }),
-);
+const args = await resolveArgs(process.argv.slice(2));
 
-// cleanup
-{
-  const dist = "dist";
-  if (fs.existsSync(dist)) await fs.promises.rm(dist, { recursive: true });
-  await fs.promises.mkdir(dist, { recursive: true });
+const dist = "dist";
+fs.existsSync(dist) && (await fs.promises.rm(dist, { recursive: true }));
+await fs.promises.mkdir(dist, { recursive: true });
+
+const toDist = (...paths: string[]) => path.join(dist, ...paths);
+if (args.flag !== "dev") await buildRenderer({ dirname: "../view" });
+await buildPreload();
+await buildMain();
+
+async function resolveArgs(args: string[]) {
+  return Object.fromEntries(
+    args
+
+      .filter((x) => x.startsWith("--"))
+      .map((x) => {
+        let [k, v] = x.split("=");
+        v ??= "true";
+        try {
+          v = JSON.parse(v);
+        } catch {}
+        return [k!.slice(2), v as any];
+      }),
+  ) as Record<string, string | number | boolean>;
 }
 
-// renderer
-if (args.flag !== "dev") {
-  const dist = "dist/renderer";
-  if (fs.existsSync(dist)) await fs.promises.rm(dist, { recursive: true });
-
+async function buildRenderer(opts: { dirname: string }) {
+  const { dirname } = opts;
   await Bun.$`
-    cd ../view
+    cd ${dirname}
     bun run build
-    mv ./dist ${path.resolve(dist)}
+    mv dist ${path.resolve(toDist("dist"))}
   `;
 }
 
-// preload
-{
-  const dist = "dist/preload";
-  if (fs.existsSync(dist)) await fs.promises.rm(dist, { recursive: true });
-
+async function buildPreload() {
   await Bun.build({
     entrypoints: ["src/preload/index.ts"],
-    outdir: dist,
+    outdir: toDist("preload"),
     target: "browser",
     format: "cjs",
     sourcemap: "external",
@@ -54,14 +53,10 @@ if (args.flag !== "dev") {
   });
 }
 
-// main
-{
-  const dist = "dist/main";
-  if (fs.existsSync(dist)) await fs.promises.rm(dist, { recursive: true });
-
+async function buildMain() {
   await Bun.build({
     entrypoints: ["src/main.ts"],
-    outdir: dist,
+    outdir: toDist("main"),
     target: "node",
     format: "esm",
     sourcemap: "external",
