b18229621cbc70af31356f4612dffa879c96a4bc
lincoln <1043156036@qq.com>
2026-01-25 18:57:00 +0800

    feat(music-converter): electron config

---
+++ apps/music-converter/scripts/electron.config.ts
@@ -0,0 +1,11 @@
+import type { Config } from "utils/electron";
+
+export const config: Config = {
+  csp: {
+    "img-src": [
+      // image
+      "http://p3.music.126.net/",
+      "http://p4.music.126.net/",
+    ],
+  },
+};
