e84b476b74dd7d48888fb9efad0ce3e2cb1b9433
lincoln <1043156036@qq.com>
2026-01-25 16:18:14 +0800

    feat: tmall tracker

---
+++ apps/tmall-tracker/src/App.tsx
@@ -0,0 +1,9 @@
+import { Button } from "ui";
+
+export default function App() {
+  return (
+    <div className="flex flex-col justify-center items-center min-h-screen">
+      <Button label="button" onClick={() => {}} />
+    </div>
+  );
+}
