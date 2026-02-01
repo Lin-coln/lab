41cd94514b7fa0dd2929be8aaeab5f6384f43a3c
lincoln <1043156036@qq.com>
2026-02-01 21:11:53 +0800

    feat(converter): avatar

--- apps/music-converter/src/App.tsx
+++ apps/music-converter/src/App.tsx
@@ -5,7 +5,8 @@ import cx from "clsx";
 import { Header } from "@/components/Header.tsx";
 import { Button } from "ui";
 import { FaPlus } from "react-icons/fa6";
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
+import { MdFace } from "react-icons/md";
 
 export default function App() {
   useInitializeEffects();
@@ -21,12 +22,7 @@ export default function App() {
 
   return (
     <>
-      <div
-        className={cx(
-          "flex flex-col w-full min-h-viewport justify-start items-center",
-          // "app-region-drag"
-        )}
-      >
+      <div className={cx("flex flex-col w-full min-h-viewport justify-start items-center")}>
         {targetArr.length ? (
           <div className={cx("max-w-3xl mx-auto w-full", "my-16 px-4", "flex flex-col gap-4")}>
             {targetArr.map((id) => {
@@ -34,14 +30,7 @@ export default function App() {
             })}
           </div>
         ) : (
-          <div
-            className={cx(
-              "app-region-drag",
-              "max-w-3xl mx-auto w-full grow",
-              "px-4",
-              "flex flex-col justify-center items-center",
-            )}
-          >
+          <div className={cx("max-w-3xl mx-auto w-full grow", "px-4", "flex flex-col justify-center items-center")}>
             <div className={cx("truncate text-xs text-neutral-50/60", "mb-4")}>
               select your audio file and convert to mp3
             </div>
@@ -58,6 +47,31 @@ export default function App() {
       </div>
 
       <Header
+        menu={
+          <>
+            <Button
+              variant="subtle"
+              className={["w-full h-fit", "p-2"]}
+              label={
+                <div className="flex flex-row w-full items-center">
+                  <Avatar />
+                  <div className="flex flex-col text-start ml-3">
+                    <div className="truncate text-sm text-neutral-50/60">LinColn</div>
+                    <div className="truncate text-xs text-neutral-50/40 font-normal">author</div>
+                  </div>
+                </div>
+              }
+              onClick={() => {
+                window.open("https://github.com/Lin-coln", "blank");
+              }}
+            />
+            <div className="text-xs p-1 text-neutral-50/60">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa facilis id tenetur. Debitis, facilis,
+              vitae? Aspernatur aut explicabo facere maiores repellendus! Beatae ipsum, minus! Ab exercitationem
+              inventore laudantium provident veniam.
+            </div>
+          </>
+        }
         actions={
           Boolean(targetArr.length) && (
             <Button
@@ -102,4 +116,29 @@ function useFilePicker(onChange: (files: FileList) => void) {
     renderInput,
     open: () => inputRef.current?.click(),
   };
+}
+
+function Avatar() {
+  const [avatar, setAvatar] = useState("");
+
+  const fallbackContent = (
+    <div className="flex items-center justify-center bg-default/50 text-foreground w-full h-full">
+      <MdFace className="text-3xl text-neutral-500" />
+    </div>
+  );
+
+  useEffect(() => {
+    void (async () => {
+      const resp = await fetch(`https://api.github.com/users/lin-coln`);
+      const { avatar_url } = await resp.json();
+      setAvatar(avatar_url);
+    })();
+  }, []);
+
+  return (
+    <div className={cx("w-8 h-8 text-large shrink-0", "rounded-full overflow-hidden")}>
+      {Boolean(avatar) && <img src={avatar} alt="avatar" />}
+      {Boolean(!avatar) && fallbackContent}
+    </div>
+  );
 }
--- apps/music-converter/src/components/Header.tsx
+++ apps/music-converter/src/components/Header.tsx
@@ -1,8 +1,11 @@
 import cx from "clsx";
 import { IoMdMusicalNote } from "react-icons/io";
-import type { ReactNode } from "react";
+import { type ReactNode, useState } from "react";
+import { Popover } from "ui";
 
-export function Header(props: { actions: ReactNode }) {
+export function Header(props: { menu: ReactNode; actions: ReactNode }) {
+  const [openMenu, setOpenMenu] = useState(false);
+
   return (
     <div
       className={cx(
@@ -13,18 +16,26 @@ export function Header(props: { actions: ReactNode }) {
         "pl-safe-area pr-safe-area",
       )}
     >
-      <div
-        className={cx(
-          "relative",
-          "rounded-md p-1",
-          "flex flex-row items-center",
-          "text-neutral-50/10",
-          "hover:bg-neutral-50/5 hover:text-neutral-50/30 transition-colors",
-        )}
-      >
-        <IoMdMusicalNote className="translate-x-1.5 -rotate-12 -ms-1.5" size={24} />
-        <div className={cx("text-base font-bold me-2")}>Converter</div>
-      </div>
+      <Popover open={openMenu} setOpen={(open) => setOpenMenu(open)} placement="bottom-start">
+        <Popover.Trigger>
+          <div
+            className={cx(
+              "relative",
+              "rounded-md p-1",
+              "flex flex-row items-center",
+              "text-neutral-50/10",
+              "hover:bg-neutral-50/5 hover:text-neutral-50/30 transition-colors",
+              "aria-expanded:bg-neutral-50/5 aria-expanded:text-neutral-50/30",
+            )}
+          >
+            <IoMdMusicalNote className="translate-x-1.5 -rotate-12 -ms-1.5" size={24} />
+            <div className={cx("text-base font-bold me-2")}>Converter</div>
+          </div>
+        </Popover.Trigger>
+        <Popover.Content as="panel" className="w-56 rounded-lg">
+          {props.menu}
+        </Popover.Content>
+      </Popover>
 
       <div className={cx("app-region-drag", "ms-auto", "flex flex-row items-center self-stretch")}>{props.actions}</div>
     </div>
